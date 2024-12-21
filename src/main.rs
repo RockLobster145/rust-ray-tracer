ff2649248f4b49b66c9599f40820042047be3f74
Stewart.N.Harvey <stewart.n.harvey@gmail.com>
2024-12-20 20:59:45 -0500

    Graphics Hello World! Produce ppm image.

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    // Define image resolution
+    let image_width = 256;
+    let image_height = 256;
+
+    println!("P3\n{} {}\n255", image_width, image_height);
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let r = i as f32 / (image_width - 1) as f32;
+            let g = j as f32 / (image_height - 1) as f32;
+            let b = 0.0f32;
+
+            let ir = (255.99 * r) as i32;
+            let ig = (255.99 * g) as i32;
+            let ib = (255.99 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+
+
+
+}
